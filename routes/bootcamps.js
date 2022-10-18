4243d3d6095076d49142ac9706bed9c7f99211ff
Edilberto Rios Garcia <dredysteam@gmail.com>
2022-10-18 00:00:30 -0400

    bootcamp controller file created

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ sucess: true, msg: 'Show all bootcamps' });
-});
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
 
-router.get('/:id', (req, res) => {
-  res.status(200).json({ sucess: true, msg: `Show bootcamp ${req.params.id}` });
-});
+router.route('/').get(getBootcamps).post(createBootcamp);
 
-router.post('/', (req, res) => {
-  res.status(200).json({ sucess: true, msg: 'Create new bootcamp' });
-});
-
-router.put('/:id', (req, res) => {
-  res.status(200).json({ sucess: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({ sucess: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
 module.exports = router;

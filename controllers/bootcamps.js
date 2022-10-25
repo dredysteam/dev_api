39bb8c9c83cb27ce25592640e39ce99b5e140392
Edilberto Rios Garcia <dredysteam@gmail.com>
2022-10-25 16:46:08 -0400

    getBootcamps controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,8 +3,13 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc  Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ sucess: true, msg: 'Show all bootcamps', hello: req.hello });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ sucess: true, data: bootcamps });
+  } catch (error) {
+    res.status(400).json({ sucess: false });
+  }
 };
 
 // @desc  Get single bootcamp
